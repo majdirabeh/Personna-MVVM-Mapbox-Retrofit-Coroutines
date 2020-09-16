a47b2aaa708ac10239f7612276a660570a5893dc
mrabeh <m.rabeh.majdi@gmail.com>
2020-09-16 17:59:48 +0200

    Add screenshots

--- app/src/main/java/fr/dev/majdi/personnacoroutines/utils/Constants.kt
+++ app/src/main/java/fr/dev/majdi/personnacoroutines/utils/Constants.kt
@@ -8,6 +8,6 @@ class Constants {
     companion object {
         const val BASE_URL = "https://randomuser.me"
         const val NUMBER_OF_USERS = 15
-        const val NATIONALITY = "gb"
+        const val NATIONALITY = "fr"
     }
 }
